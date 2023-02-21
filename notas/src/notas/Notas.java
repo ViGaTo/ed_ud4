bc06a03e7f63f82e9defd90ec89a695f12165c1f
usuario <usuario@DESKTOP-RCI5G46>
2023-02-21 13:13:12 +0100

    Refactorizado el nombre de las variables.

--- notas/src/notas/Notas.java
+++ notas/src/notas/Notas.java
@@ -6,23 +6,23 @@ public class Notas {
 
 public static void main(String[] args) {
 Scanner reader = new Scanner(System.in);// Para pedir un dato numérico por teclado
-int n = 0;
-String c = "";
+int numero = 0;
+String calificacion = "";
 System.out.print("Introduzca el valor de una calificación: ");
-n = reader.nextInt();
+numero = reader.nextInt();
 
-if (n >= 0 && n < 5)
-c = "Suspenso";
-else if (n >= 5 && n < 7)
-c = "Aprobado";
-else if (n >= 7 && n <9)
-c = "Notable";
-else if (n >= 9 && n <= 10)
-c = "Sobresaliente";
+if (numero >= 0 && numero < 5)
+calificacion = "Suspenso";
+else if (numero >= 5 && numero < 7)
+calificacion = "Aprobado";
+else if (numero >= 7 && numero <9)
+calificacion = "Notable";
+else if (numero >= 9 && numero <= 10)
+calificacion = "Sobresaliente";
 else
-c = "El valor de la calificación introducida no es correcta";
+calificacion = "El valor de la calificación introducida no es correcta";
 
-System.out.println(c);
+System.out.println(calificacion);
 reader.close();
 }
 }
