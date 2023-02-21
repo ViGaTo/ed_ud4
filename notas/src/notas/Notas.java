bbcc5440e21f3a52bb04148710113d41f33b9127
usuario <usuario@DESKTOP-RCI5G46>
2023-02-21 13:41:16 +0100

    Refactorización de código.

--- notas/src/notas/Notas.java
+++ notas/src/notas/Notas.java
@@ -11,18 +11,28 @@ String calificacion = "";
 System.out.print("Introduzca el valor de una calificación: ");
 numero = reader.nextInt();
 
-if (numero >= 0 && numero < 5)
-calificacion = "Suspenso";
-else if (numero >= 5 && numero < 7)
-calificacion = "Aprobado";
-else if (numero >= 7 && numero <9)
-calificacion = "Notable";
-else if (numero >= 9 && numero <= 10)
-calificacion = "Sobresaliente";
-else
-calificacion = "El valor de la calificación introducida no es correcta";
+calificacion = cali(numero);
 
 System.out.println(calificacion);
 reader.close();
+}
+
+/**
+ * @param numero
+ * @return
+ */
+public static String cali(int numero) {
+	String calificacion;
+	if (numero >= 0 && numero < 5)
+	calificacion = "Suspenso";
+	else if (numero >= 5 && numero < 7)
+	calificacion = "Aprobado";
+	else if (numero >= 7 && numero <9)
+	calificacion = "Notable";
+	else if (numero >= 9 && numero <= 10)
+	calificacion = "Sobresaliente";
+	else
+	calificacion = "El valor de la calificación introducida no es correcta";
+	return calificacion;
 }
 }
